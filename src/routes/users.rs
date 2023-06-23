fa3b9f61f200b2bf1e8eec5761cd80581ff8b386
Wanderrful <wanderrful@gmail.com>
2023-06-22 21:26:10 -0500

    minor refactor

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -22,24 +22,27 @@ impl UserRouter {
 
         Router::new()
             .route("/users",
-                   post(create_user)
-                       .get(list_users))
+                   post(Self::create_user)
+                       .get(Self::list_users))
             // NOTE | The state must be added at the end!
             .with_state(state)
     }
+
+    /// NOTE | The Inject<> must be the first argument! It's a quirk of shaku_axum.
+    async fn create_user(
+        user_resource: Inject<ApiModule, dyn UserResource>,
+        Json(payload): Json<CreateUserRequest>
+    ) -> (StatusCode, Json<CreateUserResponse>) {
+        user_resource.create_user(payload)
+    }
+
+    async fn list_users(
+        user_resource: Inject<ApiModule, dyn UserResource>,
+        Query(payload): Query<ListUsersRequest>
+    ) -> (StatusCode, Json<ListUsersResponse>) {
+        user_resource.list_users(payload)
+    }
+
 }
 
-/// NOTE | The Inject<> must be the first argument! It's a quirk of shaku_axum.
-async fn create_user(
-    user_resource: Inject<ApiModule, dyn UserResource>,
-    Json(payload): Json<CreateUserRequest>
-) -> (StatusCode, Json<CreateUserResponse>) {
-    user_resource.create_user(payload)
-}
 
-async fn list_users(
-    user_resource: Inject<ApiModule, dyn UserResource>,
-    Query(payload): Query<ListUsersRequest>
-) -> (StatusCode, Json<ListUsersResponse>) {
-    user_resource.list_users(payload)
-}
